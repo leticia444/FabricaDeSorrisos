79a2276aff4c8e5e5ac785b3966110a21062eebc
MARCOS VINICIUS MOURA DA SILVA <marcos.vmsilva27@senacsp.edu.br>
2026-02-03 16:57:42 -0300

    estrutura

---
+++ FabricaDeSorrisos.UI/InterfacesUI/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FabricaDeSorrisos.UI.InterfacesUI
+{
+    internal interface IAuthService
+    {
+    }
+}
---
+++ FabricaDeSorrisos.UI/Navigation/NavigationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FabricaDeSorrisos.UI.Navigation
+{
+    internal class NavigationService
+    {
+    }
+}
